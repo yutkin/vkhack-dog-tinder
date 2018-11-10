8d43ec3968d145e4bf55bc1c0ddb0a4a64c67868
Mikhail Khokhlov <hazolsky@gmail.com>
2018-11-10 14:18:15 +0300

    Remove sw

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,45 +1,85 @@
 import React from 'react';
-import connect from '@vkontakte/vkui-connect';
-import { View } from '@vkontakte/vkui';
+import { View, Epic, Tabbar, TabbarItem, Panel, PanelHeader } from '@vkontakte/vkui';
+import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
+import Icon28Search from '@vkontakte/icons/dist/28/search';
+import Icon28Message from '@vkontakte/icons/dist/28/message';
+import Icon28Notifications from '@vkontakte/icons/dist/28/notifications';
+import Icon28More from '@vkontakte/icons/dist/28/more';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
-
 class App extends React.Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
-
+    
         this.state = {
-            activePanel: 'home',
-            fetchedUser: null,
+            activeStory: 'more'
         };
+        this.onStoryChange = this.onStoryChange.bind(this);
+    }
+  
+    onStoryChange (e) {
+        this.setState({ activeStory: e.currentTarget.dataset.story })
     }
 
-    componentDidMount() {
-        connect.subscribe((e) => {
-            switch (e.detail.type) {
-                case 'VKWebAppGetUserInfoResult':
-                    this.setState({ fetchedUser: e.detail.data });
-                    break;
-                default:
-                    console.log(e.detail.type);
-            }
-        });
-        connect.send('VKWebAppGetUserInfo', {});
-    }
-
-    go = (e) => {
-        this.setState({ activePanel: e.currentTarget.dataset.to })
-    };
-
-    render() {
+    render () {
         return (
-            <View activePanel={this.state.activePanel}>
-                <Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
-                <Persik id="persik" go={this.go} />
-            </View>
-        );
+            <Epic activeStory={this.state.activeStory} tabbar={
+                <Tabbar>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'feed'}
+                        data-story="feed"
+                    ><Icon28Newsfeed /></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'discover'}
+                        data-story="discover"
+                    ><Icon28Search /></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'messages'}
+                        data-story="messages"
+                        label="12"
+                    ><Icon28Message /></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'notifications'}
+                        data-story="notifications"
+                    ><Icon28Notifications /></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'more'}
+                        data-story="more"
+                    ><Icon28More /></TabbarItem>
+                </Tabbar>
+            }>
+                <View id="feed" activePanel="feed">
+                    <Panel id="feed">
+                        <PanelHeader>Feed</PanelHeader>
+                    </Panel>
+                </View>
+                <View id="discover" activePanel="discover">
+                    <Panel id="discover">
+                        <PanelHeader>Discover</PanelHeader>
+                    </Panel>
+                </View>
+                <View id="messages" activePanel="messages">
+                    <Panel id="messages">
+                        <PanelHeader>Messages</PanelHeader>
+                    </Panel>
+                </View>
+                <View id="notifications" activePanel="notifications">
+                    <Panel id="notifications">
+                        <PanelHeader>Notifications</PanelHeader>
+                    </Panel>
+                </View>
+                <View id="more" activePanel="more">
+                    <Panel id="more">
+                        <PanelHeader>More</PanelHeader>
+                    </Panel>
+                </View>
+            </Epic>
+        )
     }
 }
 
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,12 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
 import App from './App';
-import registerServiceWorker from './sw';
+// import registerServiceWorker from './sw';
 
 // Init VK App
 connect.send('VKWebAppInit', {});
 
 // Service Worker For Cache
-registerServiceWorker();
+// registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById('root'));
